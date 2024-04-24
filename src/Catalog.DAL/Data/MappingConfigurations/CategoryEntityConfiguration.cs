42dd1de4824ba6993c8d0272df67c93572a488df
Kate <katyapopovska08@gmail.com>
2024-04-24 17:56:37 +0300

    Added entity configurations.

---
+++ src/Catalog.DAL/Data/MappingConfigurations/CategoryEntityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.DAL.DomainModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.DAL.Data.MappingConfigurations
+{
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+           builder.ToTable("Category");
+
+            builder.Property(c => c.Id)
+                .UseHiLo("category_hilo")
+                .IsRequired();
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
---
+++ src/Catalog.DAL/Data/MappingConfigurations/ProductPhotoEntityConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.DAL.DomainModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.DAL.Data.MappingConfigurations
+{
+    public class ProductPhotoEntityConfiguration : IEntityTypeConfiguration<ProductPhoto>
+    {
+        public void Configure(EntityTypeBuilder<ProductPhoto> builder)
+        {
+            builder.ToTable("ProductPhoto");
+
+            builder.Property(x => x.Id)
+                .UseHiLo("photo_hilo")
+                .IsRequired();
+
+            builder.Property(x => x.Source)
+                .IsRequired();
+
+            builder.HasOne(x => x.Product)
+                .WithMany(x => x.ProductPhotos)
+                .HasForeignKey(x => x.ProductId);
+        }
+    }
+}
