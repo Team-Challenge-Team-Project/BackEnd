6a08cbf4da91c75a7cd9fa899d34a9cb45fb5f28
Kate <katyapopovska08@gmail.com>
2024-04-24 16:44:14 +0300

    Added entities.

---
+++ src/Catalog.DAL/DomainModels/Size.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.DAL.DomainModels
+{
+    public class Size
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
