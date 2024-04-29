49303a2744b6c9f6cc0680881c974dfaad95200f
Kate <katyapopovska08@gmail.com>
2024-04-29 20:21:21 +0300

    Added sex enum property to Product entity.

---
+++ src/Catalog.DAL/Models/Enums/ProductSex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.DAL.Models.Enums
+{
+    public enum ProductSex
+    {
+        Male,
+        Female,
+        Unisex
+    }
+}
