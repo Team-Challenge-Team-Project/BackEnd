5e03e4df4000d493dbeb140e5ad97cce4bf2eb06
Kate <katyapopovska08@gmail.com>
2024-05-01 19:16:17 +0300

    Implement generic repository.

---
+++ src/Catalog.DAL/GenericRepository/Base/GenericRepository.cs
@@ -0,0 +1,69 @@
+﻿using Catalog.DAL.Data;
+using Catalog.DAL.GenericRepository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.DAL.GenericRepository.Base
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private CatalogDbContext _context = null;
+        private DbSet<TEntity> _dbSet = null;
+
+        public GenericRepository(CatalogDbContext context)
+        {
+           _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public TEntity GetById(object id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public async Task<TEntity> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void HardDelete(TEntity entity)
+        {
+           _dbSet.Remove(entity);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public async Task InsertAsync(TEntity entity)
+        {
+           await _dbSet.AddAsync(entity);
+        }
+
+        public void SoftDelete(TEntity entity)
+        {
+            entity.GetType().GetProperty("IsDeleted")?.SetValue(entity, true);
+            _dbSet.Update(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+    }
+}
---
+++ src/Catalog.DAL/GenericRepository/Interfaces/IGenericRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.DAL.GenericRepository.Interfaces
+{
+    /// <summary>
+    /// This interface implemented base database operation with generic repository pattern
+    /// </summary>
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// Retrieves all entities of type <typeparamref name="TEntity"/>
+        /// </summary>
+        /// <returns>
+        /// An enumerable collection of all entities of type <typeparamref name="TEntity"/>
+        /// </returns>
+        IEnumerable<TEntity> GetAll();
+
+        /// <summary>
+        /// Retrieves all entities async of type <typeparamref name="TEntity"/>
+        /// </summary>
+        /// <returns>
+        /// An enumerable collection of all entities of type <typeparamref name="TEntity"/>
+        /// </returns>
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        /// <summary>
+        /// Retrieves entity by id of type <typeparamref name="TEntity"/>
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>
+        /// Returns an entity of type <typeparamref name="TEntity"/>
+        /// </returns>
+        TEntity GetById(object id);
+
+        /// <summary>
+        /// Retrieves entity by id async of type <typeparamref name="TEntity"/>
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>
+        /// Returns an entity of type <typeparamref name="TEntity"/>
+        /// </returns>
+        Task<TEntity> GetByIdAsync(object id);
+
+        /// <summary>
+        /// Inserts the specified entity of type <typeparamref name="TEntity"/>
+        /// </summary>
+        /// <param name="entity"></param>
+        void Insert(TEntity entity);
+
+
+        /// <summary>
+        /// Inserts the specified entity of type <typeparamref name="TEntity"/>
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task InsertAsync(TEntity entity);
+
+        /// <summary>
+        /// Updates the specified entity of type <typeparamref name="TEntity"/>.
+        /// </summary>
+        /// <param name="entity"></param>
+        void Update(TEntity entity);
+
+        /// <summary>
+        /// Soft delete with using IsDeleted flag for entity.
+        /// </summary>
+        /// <param name="entity"></param>
+        void SoftDelete(TEntity entity);
+
+        /// <summary>
+        /// Deletes the specified entity.
+        /// </summary>
+        /// <param name="entity"></param>
+        void HardDelete(TEntity entity);
+    }
+}
