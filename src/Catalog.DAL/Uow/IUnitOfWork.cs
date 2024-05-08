71a19479290fbbc81850c9c6e0a2af226966b489
Kate <katyapopovska08@gmail.com>
2024-05-08 18:00:11 +0300

    Implement unit of work pattern.

---
+++ src/Catalog.DAL/Uow/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.DAL.Uow
+{
+    public interface IUnitOfWork
+    {
+        int Commit();
+        Task<int> CommitAsync();
+        IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+    }
+}
